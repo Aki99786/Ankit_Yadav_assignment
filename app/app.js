68ec1e025ac234ba82bd7b27bd2e1ddd2bb0906c
Ankit Yadav <aki99786@gmail.com>
2026-05-18 10:34:04 +0530

    Configure Prometheus metrics, Grafana dashboards, and cAdvisor monitoring

--- app/app.js
+++ app/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const client = require("prom-client");
+
+// Collect default system metrics (CPU, memory, etc.)
+client.collectDefaultMetrics();
 
 const app = express();
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
 
 app.get("/", (req, res) => {
     res.send("CI/CD Pipeline Working Successfully");
